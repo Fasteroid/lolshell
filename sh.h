207e829ddc967f59f3b8fdd0b48f71520b89408c
Fasteroid <fasteroid@hotmail.com>
2021-10-06 23:21:37 -0400

    fix which, implement where and cd

--- sh.h
+++ sh.h
@@ -4,9 +4,10 @@
 int pid;
 int sh( int argc, char **argv, char **envp);
 char *which(char *command, struct pathelement *pathlist);
-char *where(char *command, struct pathelement *pathlist);
+void where(char *command, struct pathelement *pathlist);
 void list ( char *dir );
 void printenv(char **envp);
+void dircat(char *str1, char *str2);
 
 #define PROMPTMAX 32
 #define MAXARGS 10
