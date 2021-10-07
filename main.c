10356123227dda25712659cf9e875c6661de59d8
Fasteroid <fasteroid@hotmail.com>
2021-10-07 16:30:46 -0400

    signal stuff

--- main.c
+++ main.c
@@ -2,17 +2,34 @@
 #include <signal.h>
 #include <stdio.h>
 
-void sig_handler(int signal); 
+void sig_handler(int sig){
+    switch (sig){
+        case SIGTERM:
+            write(STDOUT_FILENO,"\n",2);
+        break;
+
+        case SIGINT:
+            // thx stackoverflow: https://stackoverflow.com/questions/16891019/how-to-avoid-using-printf-in-a-signal-handler
+            write(STDOUT_FILENO,"\n",2);
+            printprompt();
+        break;
+
+        case SIGTSTP:
+            write(STDOUT_FILENO,"\n",2);
+            printprompt();
+        break;
+        
+        default:
+        break;
+    }
+    signal(SIGINT,sig_handler);
+}
 
 int main( int argc, char **argv, char **envp )
 {
-    /* put signal set up stuff here */
-
+    signal(SIGINT,sig_handler); // will this work?
+    signal(SIGTSTP,sig_handler); // will this work?
     return sh(argc, argv, envp);
 }
 
-void sig_handler(int signal)
-{
-    /* define your signal handler */
-}
-
+// I prefer to put functions at the beginning
--- sh.h
+++ sh.h
@@ -2,12 +2,15 @@
 #include "get_path.h"
 
 int pid;
+char *pwd, *prompt;
+
 int sh( int argc, char **argv, char **envp);
 char *which(char *command, struct pathelement *pathlist);
 void where(char *command, struct pathelement *pathlist);
 void list ( char *dir );
 void printenv(char **envp);
 void dircat(char *str1, char *str2);
+void printprompt();
 
 #define PROMPTMAX 32
 #define MAXARGS 10
